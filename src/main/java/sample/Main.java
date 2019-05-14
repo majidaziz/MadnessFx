5fc58280453ee08eace125e184bd9828e7b01916
majidaziz <aziz112@mail.chapman.edu>
2019-05-14 00:58:27 -0700

    added shit, cleaned shit, fixed shit.

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,3 +1,9 @@
+/*
+* WE DID EVERYTHING IN MAIN LMAO
+* Authors: Jayson Marshall, Majid Aziz
+* Chapman University
+*
+* */
 package sample;
 
 import javafx.application.Application;
@@ -7,61 +13,89 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import sun.reflect.annotation.ExceptionProxy;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.List;
+import java.sql.*;
 
 public class Main extends Application {
 
-    ListView listView;
-    Pane root;
+    Button btnReport, btnDelete, btnInsert, btnUpdate, btn64, btn32, btn16, btn8, btn4, btn2, btn1;
+    ListView listView, listView2;
+    TextField textField3, textField2, textField1, textField;
     Connection myConn;
     Statement myStmt;
-
+    Pane root;
 
     @Override
     public void start(final Stage primaryStage) {
+        // Connect to database, dont forget "USE MarchMadness";
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             myConn = DriverManager.getConnection("jdbc:mysql://35.236.126.183:3306/MarchMadness","root", "password");
             myStmt = myConn.createStatement();
 
-            int rs = myStmt.executeUpdate("UPDATE Rounds SET finalFour = 1 WHERE teamID = 15");
-            //while(rs.next()) {
-            //    System.out.println("ID: " + rs.getString(2));
-            //}
+            //int rs = myStmt.executeUpdate("UPDATE Players SET teamID = 1 WHERE name = 'Zion Williamson'");
         }catch(Exception e){System.out.println(e);}
-        Label lbl = new Label("MARCH MADNESS BITCH");
-        Button btn64 = new Button("First Round");
-        Button btn32 = new Button("Second Round");
-        Button btn16 = new Button("Sweet Sixteen");
-        Button btn8 = new Button("Elite Eight");
-        Button btn4 = new Button("Final Four");
-        Button btn2 = new Button("National Championship");
-        Button btn1 = new Button("Champions");
+
+        Label lbl = new Label("MARCH MADNESS 2019");
+        btnUpdate = new Button("Update Player");
+        btnInsert = new Button("Add Player");
+        btnDelete = new Button("Del Player");
+        btnReport = new Button("Report");
+        btn64 = new Button("First Round");
+        btn32 = new Button("Second Round");
+        btn16 = new Button("Sweet Sixteen");
+        btn8 = new Button("Elite Eight");
+        btn4 = new Button("Final Four");
+        btn2 = new Button("National Championship");
+        btn1 = new Button("Champions");
+        textField3 = new TextField();
+        textField2 = new TextField();
+        textField1 = new TextField();
+        textField = new TextField();
+        listView2 = new ListView();
         listView = new ListView();
 
-        listView.setPrefSize(320,480);
-        listView.getItems().add("fuck");
+
+        listView.setPrefSize(310,460);
+        listView2.setPrefSize(300, 140);
+
+        /* Button shit till line ? //comeback to this */
         listView.setOnMouseClicked(new EventHandler<MouseEvent>() {
             public void handle(MouseEvent event) {
-                System.out.println("click fkdfs");
+                System.out.println(listView.getSelectionModel().getSelectedItem());
+                String team_Name = listView.getSelectionModel().getSelectedItem().toString();
+                listView2.getItems().clear();
+                try {
+                    PreparedStatement ps = myConn.prepareStatement("SELECT * FROM Teams WHERE teamName = ?");
+                    ps.setString(1, team_Name);
+                    ResultSet rs = ps.executeQuery();
+                    while(rs.next())
+                        listView2.getItems().add("Team: " + rs.getString(2));
+
+                }catch(Exception e){System.out.println("E: " + e);}
+                try{
+                    PreparedStatement ps = myConn.prepareStatement("SELECT conferenceName FROM Conferences c JOIN Teams t ON c.id = t.confId WHERE t.teamName = ?");
+                    ps.setString(1, team_Name);
+                    ResultSet rs = ps.executeQuery();
+                    while(rs.next())
+                        listView2.getItems().add("Conference: " + rs.getString(1));
+                }catch(Exception e){}
+                try{
+                    PreparedStatement ps = myConn.prepareStatement("SELECT name FROM Players p JOIN Teams t ON p.teamID = t.id WHERE t.teamName = ?");
+                    ps.setString(1, team_Name);
+                    ResultSet rs = ps.executeQuery();
+                    while(rs.next())
+                        listView2.getItems().add("Player: " + rs.getString(1));
+                }catch(Exception e){}
             }
         });
         // First Round
         btn64.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
-                System.out.println("please work");
                 listView.getItems().clear();
                 try {
                     ResultSet rs = myStmt.executeQuery("SELECT * FROM Teams");
@@ -130,6 +164,7 @@ public class Main extends Application {
                     {
                         listView.getItems().add(rs.getString(1));
                     }
+                    rs.close();
                 }catch(Exception e){}
             }
         });
@@ -145,10 +180,59 @@ public class Main extends Application {
                 }catch(Exception e){}
             }
         });
-        root = new Pane();
-        lbl.setLayoutX(330);
-        lbl.setLayoutY(50);
+        //Need nodeJaayson help
+        btnInsert.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                System.out.println("jayson you're my only hope");
+                /*try{
+                    PreparedStatement ps = myConn.preparedStatement("INSERT INTO Players ()")
+                }*/
+            }
+        });
+        //JASYON
+        btnDelete.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                System.out.println("jasyon durulu");
+            }
+        });
+        //fk this im done
+        btnUpdate.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+                System.out.println("im hungry but tired");
+            }
+        });
+        //this is gonna suck
+        btnReport.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent event) {
+
+            }
+        });
+
+        /*Lay it out //Game of Thrones season 8 is a disappointment*/
         lbl.setStyle("-fx-font: 24 arial;");
+        lbl.setLayoutX(330);
+        lbl.setLayoutY(20);
+        textField3.setLayoutX(482);
+        textField3.setLayoutY(190);
+        textField3.setPromptText("current name");
+        textField2.setLayoutX(482);
+        textField2.setLayoutY(220);
+        textField2.setPromptText("new name");
+        textField1.setLayoutX(482);
+        textField1.setLayoutY(130);
+        textField1.setPromptText("enter team, mind capitals");
+        textField.setLayoutX(482);
+        textField.setLayoutY(100);
+        textField.setPromptText("enter player");
+        btnUpdate.setPrefWidth(148);
+        btnUpdate.setLayoutX(482);
+        btnUpdate.setLayoutY(250);
+        btnDelete.setLayoutX(482);
+        btnDelete.setLayoutY(160);
+        btnInsert.setLayoutX(557);
+        btnInsert.setLayoutY(160);
+        btnReport.setLayoutX(578);
+        btnReport.setLayoutY(280);
         btn64.setLayoutX(330);
         btn64.setLayoutY(100);
         btn32.setLayoutX(330);
@@ -163,7 +247,15 @@ public class Main extends Application {
         btn2.setLayoutY(250);
         btn1.setLayoutX(330);
         btn1.setLayoutY(280);
-        root.getChildren().addAll(btn64, btn32, btn16, btn8, btn4, btn2, btn1, lbl, listView);
+        listView2.setLayoutX(330);
+        listView2.setLayoutY(330);
+        listView.setLayoutX(10);
+        listView.setLayoutY(10);
+
+        /*Set the stage*/
+        root = new Pane();
+        root.getChildren().addAll(btnReport, btnUpdate, btnDelete, btnInsert, btn64, btn32, btn16, btn8, btn4, btn2, btn1, lbl,
+                textField3, textField2, textField1, textField, listView, listView2);
         primaryStage.setScene(new Scene(root, 640, 480));
         primaryStage.show();
     }
